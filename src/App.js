c22ff82f523e2440846b6410d93d34031df7fe6a
fescobar-2 <39492866+fescobar-2@users.noreply.github.com>
2019-08-14 17:38:24 -0400

    Added face recog app files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Logo/>
+      <ImageLinkForm/>
+      {/* <FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
